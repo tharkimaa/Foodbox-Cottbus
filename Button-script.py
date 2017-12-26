fe65b766723963547738139928dc7c1373d608f3
Arjun Jamil <jamil@Arjuns-MacBook-Pro.local>
2017-12-26 12:34:08 +0100

    Improvements

--- Button-script.py
+++ Button-script.py
@@ -5,7 +5,7 @@ from gpiozero import Button
 import time
 import subprocess
 from signal import pause
-from datetime import datetime
+
 
 logtext = ""
 
@@ -14,14 +14,14 @@ def putinlog(logtext):
             log.write('/n' + logtext)
             log.close()
 def button_press():
-        datetime = datetime.now().isoformat()
+
         putinlog("Button pressed")
         subprocess.call(["raspistill", "-o", "/home/pi/testpics/Latest.jpg"])
         subprocess.call(["mpack", "-s", "[Test] Hey there! Someone just put this in the foodbox. Come and get it before it's gone!", "/home/pi/testpics/Latest.jpg", "trigger@applet.ifttt.com"])
         putinlog("Image posted")
 
 def say_bye():
-        datetime = datetime.now().isoformat()
+
         putinlog("Button released")
 
 button = Button(21)
